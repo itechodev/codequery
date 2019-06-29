1aafa38cda4e3cbc85b8d671ff2c3f388d3c8326
Willem Bijker <willem@itecho.co.za>
2019-06-29 10:19:27 +0200

    Add random thinking notes

--- FieldExpression.cs
+++ FieldExpression.cs
@@ -1,3 +1,27 @@
+// Select coalesce( datetime('now') > null, 0), 'str' == 'aa', (select 10) + 10
+
+//  SELECT
+//     [Field...]
+// FROM
+//     [SOURCE]
+// JOIN [SOURCE] on [ConditionalExpression...]
+// WHERE [ConditionalExpression...] 
+// ORDER BY [FieldName...] ASC/DESC
+// GROUP BY [FieldName...]
+// HAVING [HavingExpression] 
+
+// ConditionalExpression: [Field] [Operator] [Field]
+// Field: FieldConstant, FieldName, FieldAggregate, FieldRowFunction, FieldExpression
+// FieldExpression: Func([Field], [Field] [Operator] [Field]
+// HavingExpression: [FieldAggregate] [Operator] [Field]
+
+// FieldSelectExpression: FieldConstant, FieldName, FieldAggregate, FieldRowFunction, FieldOperator, FieldBoolean, FieldExpression
+// FieldWhere: FieldBoolean 
+
+
+// FieldMath
+// FieldFunction
+
 namespace codequery
 {
     public class QuerySource
