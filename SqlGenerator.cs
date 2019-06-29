f127de4b2088cc56f8565bd5e6c597c56bbd1d02
Willem Bijker <willem@itecho.co.za>
2019-06-29 16:29:07 +0200

    Add  generator for beautiful and consitent SQL

---
+++ SqlGenerator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text;
+
+namespace codequery
+{
+    public class SqlGenerator
+    {
+        private StringBuilder builder;
+        private int indent;
+
+        public SqlGenerator()
+        {
+            builder = new StringBuilder();
+            indent = 0;
+        }
+
+        public void Indent()
+        {
+            indent++;
+        }
+
+        public void UnIndent()
+        {
+            indent--;
+        }
+        
+        public void AddLine(string line)
+        {
+            builder.Append(new String(' ', indent * 4) + line);
+            builder.AppendLine();
+        }
+
+        public string Generate()
+        {
+            return builder.ToString();
+        }
+    }
+}
