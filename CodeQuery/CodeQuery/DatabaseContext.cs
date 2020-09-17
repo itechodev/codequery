fda424fb8f212c4afa983af34ca8ba5eb2b1f17e
willem-mbp <willem@itecho.co.za>
2020-09-17 20:31:09 +0200

    Context populate definitions and store in cache

--- CodeQuery/CodeQuery/DatabaseContext.cs
+++ CodeQuery/CodeQuery/DatabaseContext.cs
@@ -1,18 +1,58 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using CodeQuery.Definitions;
 using CodeQuery.Interfaces;
 
 namespace CodeQuery
 {
     public abstract class DatabaseContext
     {
+        private static Dictionary<Type, SqlTableDefinition> _definitions;
+        
         public IDbQueryable<TFields> Const<TFields>(Func<TFields> fields)
         {
             return null;
         }
 
+        // extract the IDTable type if derived from generic IDbTable<T> 
+        private static Type GetTableType(Type type)
+        {
+            var interfaces = type.GetInterfaces();
+            if (interfaces?.FirstOrDefault() == typeof(IDbTable<>))
+            {
+                return type.GenericTypeArguments.FirstOrDefault();
+            }
+
+            return null;
+        }
+
         protected void Initialize()
         {
+            // share cache between multiple instances of DatabaseContext
+            if (_definitions != null)
+            {
+                return;
+            }
+            
             // Read all DbTables from this instance and convert this into table definitions
+            _definitions = new Dictionary<Type, SqlTableDefinition>();
+            foreach (var prop in GetType().GetProperties(BindingFlags.Public))
+            {
+                if (!prop.CanRead)
+                {
+                    continue;
+                }
+
+                // extract the first generic type from IDbTable<..>
+                var tableType = GetTableType(prop.PropertyType);
+                if (tableType != null)
+                {
+                    _definitions[tableType] = new SqlTableDefinition(tableType);
+                }
+            }
         }
     }
 }
