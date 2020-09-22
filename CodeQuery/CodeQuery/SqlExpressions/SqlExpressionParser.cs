d3d10e6878337f13411ab7b634f30626d92e7fbc
Willem Bijker <willem@itecho.co.za>
2020-09-22 17:46:44 +0200

    SqlExpression add member expressions

--- CodeQuery/CodeQuery/SqlExpressions/SqlExpressionParser.cs
+++ CodeQuery/CodeQuery/SqlExpressions/SqlExpressionParser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq.Expressions;
+using System.Reflection;
 using CodeQuery.Definitions;
 
 namespace CodeQuery.SqlExpressions
@@ -14,9 +15,52 @@ namespace CodeQuery.SqlExpressions
                     return ParseConstant(constant);
                 case BinaryExpression binary:
                     return ParseBinaryExpression(binary);
+                case MemberExpression member:
+                    return ParseMemberExpression(member);
                 default:
                     throw new NotImplementedException();
             }
+        }
+
+        private static SqlExpression ParseMemberExpression(MemberExpression member)
+        {
+            // if (member.NodeType == ExpressionType.MemberAccess)
+            // Maybe there are anything other than memberAccess
+            if (member.NodeType != ExpressionType.MemberAccess)
+            {
+                throw new SqlExpressionException(
+                    $"Could not translate {member.Expression} to a SQL expression. Expression type is of {member.NodeType}.");
+            }
+            
+            // Date functions
+            if (member.Member.ReflectedType == typeof(System.DateTime))
+            {
+                switch (member.Member.Name)
+                {
+                    case "Now":
+                        return new SqlFunctionExpression(SqlFunctionType.CurrentTimeStamp, null);
+                }
+            }
+
+            // Math functions
+            if (member.Member.ReflectedType == typeof(System.Math))
+            {
+                
+            }
+            
+            // String functions
+            if (member.Member.ReflectedType == typeof(string))
+            {
+                
+            }
+            
+            // else it a access to a field (column)
+            return new SqlColumnExpression(GetReferenceFieldDef(member.Member));
+        }
+
+        private static SqlColumnDefinition GetReferenceFieldDef(MemberInfo memberMember)
+        {
+            throw new NotImplementedException();
         }
 
         private static SqlExpression ParseBinaryExpression(BinaryExpression binary)
